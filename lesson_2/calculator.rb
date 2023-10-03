6ea5264a4db885f5e92a2f0ba48a64df09d5fb7f
Philippe Ngom <philngom@gmail.com>
2023-10-03 03:17:19 -0700

    made prompt method to make prompts stand out

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -3,14 +3,18 @@
 # perform operation
 # return result
 
+def prompt(message)
+  puts("=> #{message}")
+end
+
 puts "Welcome to calculator!"
-puts "What's your first number?"
+prompt("What's your first number?")
 num1 = gets.chomp.to_i
-puts "What's your second number?"
+prompt("What's your second number?")
 num2 = gets.chomp.to_i
 
-puts "What operation would you like to perform?"
-puts "1) add 2) subtract 3) multiply 4) division"
+prompt("What operation would you like to perform?")
+prompt("1) add 2) subtract 3) multiply 4) division")
 operation = gets.chomp.to_i
 
 result = nil
