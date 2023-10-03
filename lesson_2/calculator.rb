76f63e4240dbd3ab3b0c931992f57a78c0aa39c8
Philippe Ngom <philngom@gmail.com>
2023-10-03 03:34:42 -0700

    user input check for valid input refactor

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -13,45 +13,54 @@ end
 
 puts "Welcome to calculator!"
 
-num1 = ''
 loop do
-  prompt("What's your first number?")
-  num1 = gets.chomp.to_i
+  num1 = ''
+  loop do
+    prompt("What's your first number?")
+    num1 = gets.chomp.to_i
 
-  if valid_number?(num1)
-    break
-  else
-    prompt("Uh oh! Please enter a valid number.")
-  end
-end
-
-num2 = ''
-loop do
-    prompt("What's your second number?")
-    num2 = gets.chomp.to_i
-
-    if valid_number?(num2)
+    if valid_number?(num1)
       break
     else
       prompt("Uh oh! Please enter a valid number.")
     end
+  end
+
+  num2 = ''
+  loop do
+      prompt("What's your second number?")
+      num2 = gets.chomp.to_i
+
+      if valid_number?(num2)
+        break
+      else
+        prompt("Uh oh! Please enter a valid number.")
+      end
+  end
+
+  prompt("What operation would you like to perform?")
+  prompt("1) add 2) subtract 3) multiply 4) division")
+  operation = gets.chomp.to_i
+
+  result = nil
+
+  case operation
+  when 1
+    result = num1 + num2
+  when 2
+    result = num1 - num2
+  when 3
+    result = num1 * num2
+  when 4
+    result = num1.to_f / num2
+  end
+
+  puts "The result is: #{result}"
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)");
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+
 end
 
-prompt("What operation would you like to perform?")
-prompt("1) add 2) subtract 3) multiply 4) division")
-operation = gets.chomp.to_i
-
-result = nil
-
-case operation
-when 1
-  result = num1 + num2
-when 2
-  result = num1 - num2
-when 3
-  result = num1 * num2
-when 4
-  result = num1.to_f / num2
-end
-
-puts "The result is: #{result}"
+prompt("Thank you for using the calculator. Goodbye!")
