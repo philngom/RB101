1f28355e59a24b2aca246b036abe6975e663aa80
Philippe Ngom <philngom@gmail.com>
2023-10-29 00:05:24 -0700

    extra features complete rubocop happy

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -5,17 +5,16 @@ def prompt(message)
 end
 
 def win?(first, second)
-    win_obj = {
-      rock: ['scissors', 'lizard'],
-      paper: ['rock', 'spock'],
-      scissors: ['paper', 'lizard'],
-      spock: ['rock', 'scissors'],
-      lizard: ['spock', 'paper']
-    }
+  win_obj = {
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spock'],
+    scissors: ['paper', 'lizard'],
+    spock: ['rock', 'scissors'],
+    lizard: ['spock', 'paper']
+  }
 
-    win_obj[first.to_sym].include?(second)
+  win_obj[first.to_sym].include?(second)
 end
-
 
 def display_results(user, computer)
   if win?(computer, user)
@@ -41,9 +40,9 @@ loop do
   user_choice = ''
   loop do
     prompt(choice_prompt)
-    user_choice = gets.chomp.to_i - 1
-    puts user_choice
-    if VALID_CHOICES[user_choice]
+    user_choice = VALID_CHOICES[gets.chomp.to_i - 1]
+
+    if user_choice
       break
     else
       prompt("That is not a valid choice.")
@@ -52,9 +51,9 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  prompt("You chose #{VALID_CHOICES[user_choice]} and computer chose #{computer_choice}")
+  prompt("You chose #{user_choice} and computer chose #{computer_choice}")
 
-  display_results(VALID_CHOICES[user_choice], computer_choice)
+  display_results(user_choice, computer_choice)
 
   prompt("Would you like to play again?")
   break unless gets.chomp.downcase.start_with?('y')
